faa2f0bfc96e84dceb139177b1d02964d421a505
HienNguyen03 <lily31392@gmail.com>
2017-07-11 02:22:28 +0300

    Add passport

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -3,6 +3,11 @@ const jwt = require('jwt-simple');
 const config = require('../config');
 const User = require('../models/user');
 
+function tokenForUser (user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({sub: user.id, iat: timestamp}, config.secret);
+}
+
 exports.signup = function (req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
@@ -30,7 +35,7 @@ exports.signup = function (req, res, next) {
       if (err) {return next(err);}
 
       //Response to request indicating the user was created
-      res.json(user);
+      res.json({token: tokenForUser(user)});
     });
   });
 }
---
+++ services/passport.js
@@ -0,0 +1,41 @@
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const JwtStrategy = require('passport-jwt').Strategy;
+const LocalStrategy = require('passport-local');
+const passport = require('passport');
+
+const config = require('../config');
+const User = require('../models/user');
+
+// Create local Strategy
+const localOption = {
+  usernameField: 'email'
+};
+const localLogin = new LocalStrategy(localOption, function (email, password, done) {
+  // Verify these username and password, call done with username if they are correct
+  //otherwise call done with false
+});
+
+// Set up options for Jwt Strategy
+const jwtOption = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
+
+// Create JwtStrategy
+const jwtLogin = new JwtStrategy(jwtOption, function (payload, done) {
+  // See if user ID in the payload exist in the database
+  // If it does, call done with that user
+  // If not, call done without user
+  User.findById(payload.sub, function (err, user) {
+    if (err) {return done(err, false);}
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+// Tell passport to use strategy
+passport.use(jwtLogin);
